701bb1ec4f19c012790e7eedcf7a799177293070
unknown <whitebeard086@gmail.com>
2022-05-11 21:26:48 +0100

    hotel controller

---
+++ api/controllers/hotelController.js
@@ -0,0 +1,57 @@
+import Hotel from "../models/Hotel.js";
+
+// Create Hotel
+export const createHotel = async (req, res, next) => {
+  const newHotel = new Hotel(req.body);
+
+  try {
+    const savedHotel = await newHotel.save();
+    res.status(201).json(savedHotel);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Update Hotel
+export const updateHotel = async (req, res, next) => {
+  try {
+    const updatedHotel = await Hotel.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    res.status(200).json(updatedHotel);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Delete Hotel
+export const deleteHotel = async (req, res, next) => {
+  try {
+    await Hotel.findByIdAndDelete(req.params.id);
+    res.status(200).json("Hotel has been deleted!");
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Get Hotel
+export const getHotel = async (req, res, next) => {
+  try {
+    const hotel = await Hotel.findById(req.params.id);
+    res.status(200).json(hotel);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Get All Hotels
+export const getHotels = async (req, res, next) => {
+  try {
+    const hotels = await Hotel.find();
+    res.status(200).json(hotels);
+  } catch (error) {
+    next(error);
+  }
+};
