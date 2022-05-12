00869ba7382c2f2e8174bb7d1620085a73c6f0b4
unknown <whitebeard086@gmail.com>
2022-05-12 15:53:21 +0100

    room model and controller

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,7 +1,28 @@
 import express from "express";
+import {
+  createRoom,
+  updateRoom,
+  deleteRoom,
+  getRoom,
+  getRooms,
+} from "../controllers/roomController.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
+// Create
+router.post("/:hotelId", verifyAdmin, createRoom);
 
+// update
+router.put("/:id", verifyAdmin, updateRoom);
+
+// Delete
+router.delete("/:id/:hotelId", verifyAdmin, deleteRoom);
+
+// Get
+router.get("/:id", getRoom);
+
+// Get All
+router.get("/", getRooms);
 
 export default router;
