2814229498278fdfd1c073ed15263e3c5ad7070f
unknown <whitebeard086@gmail.com>
2022-05-13 17:59:18 +0100

    featured properties component

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,4 +1,5 @@
 export { default as Featured } from "./featured/Featured";
+export { default as FeaturedProperties } from "./featuredProperties/FeaturedProperties";
 export { default as Header } from "./header/Header";
 export { default as Navbar } from "./navbar/Navbar";
 export { default as PropertyList } from "./propertyList/PropertyList";
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,4 +1,4 @@
-import { Featured, Header, Navbar, PropertyList } from "../../components";
+import { Featured, FeaturedProperties, Header, Navbar, PropertyList } from "../../components";
 import "./home.css";
 
 const Home = () => {
@@ -10,6 +10,8 @@ const Home = () => {
         <Featured />
         <h1 className="homeTitle">Browse by property type</h1>
         <PropertyList />
+        <h1 className="homeTitle">Homes guests love</h1>
+        <FeaturedProperties />
       </div>
     </div>
   );
