e9e359cf0f2f13a9a637e1fa96c0de90f76f75ff
unknown <whitebeard086@gmail.com>
2022-05-13 17:23:02 +0100

    property list component

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Featured } from "./featured/Featured";
 export { default as Header } from "./header/Header";
 export { default as Navbar } from "./navbar/Navbar";
+export { default as PropertyList } from "./propertyList/PropertyList";
