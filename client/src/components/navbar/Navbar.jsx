7f3b7818a37f1f798e882e0f8c18a64b9c3af713
unknown <whitebeard086@gmail.com>
2022-05-17 18:04:16 +0100

    featured properties - api and client

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,10 +1,13 @@
+import { Link } from "react-router-dom";
 import "./navbar.css";
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navContainer">
-        <span className="logo">Booking.me</span>
+        <Link to="/" style={{ color: "inherit", textDecoration: "none"}}>
+          <span className="logo">Booking.me</span>
+        </Link>
         <div className="navItems">
           <button className="navButton">Register</button>
           <button className="navButton">Login</button>
