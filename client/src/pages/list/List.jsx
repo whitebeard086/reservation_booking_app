42724db2d823a4c2e71cd41101c034097401a65d
unknown <whitebeard086@gmail.com>
2022-05-18 08:43:25 +0100

    property page - client

--- client/src/pages/list/List.jsx
+++ client/src/pages/list/List.jsx
@@ -13,14 +13,16 @@ const List = () => {
   const [date, setDate] = useState(location.state.date);
   const [options, setOptions] = useState(location.state.options);
   const [openDate, setOpenDate] = useState(false);
-  const [min, setMin] = useState(undefined)
-  const [max, setMax] = useState(undefined)
+  const [min, setMin] = useState(undefined);
+  const [max, setMax] = useState(undefined);
 
-  const { data, loading, error, refetch } = useFetch(`/hotels?city=${destination}&min=${min || 0}&max=${max || 999}`);
+  const { data, loading, error, refetch } = useFetch(
+    `/hotels?city=${destination}&min=${min || 0}&max=${max || 999}`
+  );
 
   const handleClick = () => {
-    refetch()
-  }
+    refetch();
+  };
 
   return (
     <div>
@@ -57,13 +59,21 @@ const List = () => {
                   <span className="lsOptionText">
                     Min price <small>per night</small>
                   </span>
-                  <input type="number" onChange={(e) => setMin(e.target.value)} className="lsOptionInput" />
+                  <input
+                    type="number"
+                    onChange={e => setMin(e.target.value)}
+                    className="lsOptionInput"
+                  />
                 </div>
                 <div className="lsOptionItem">
                   <span className="lsOptionText">
                     Max price <small>per night</small>
                   </span>
-                  <input type="number" onChange={(e) => setMax(e.target.value)} className="lsOptionInput" />
+                  <input
+                    type="number"
+                    onChange={e => setMax(e.target.value)}
+                    className="lsOptionInput"
+                  />
                 </div>
                 <div className="lsOptionItem">
                   <span className="lsOptionText">Adults</span>
